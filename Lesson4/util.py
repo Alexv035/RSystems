f7019b5e1e6df17d6a34c911e10dac3617913c48
Alexander <59419330+Alexv035@users.noreply.github.com>
2021-05-20 20:19:15 +0300

    Update util.py

--- Lesson4/util.py
+++ Lesson4/util.py
@@ -1,6 +1,6 @@
 
 
-def prefilter_items(data):
+def prefilter_items(data, , item_features=None, take_n_popular=5000):
     # Уберем самые популярные товары (их и так купят)
     popularity = data_train.groupby('item_id')['user_id'].nunique().reset_index() / data_train['user_id'].nunique()
     popularity.rename(columns={'user_id': 'share_unique_users'}, inplace=True)
@@ -17,15 +17,25 @@ def prefilter_items(data):
     data = data[~data['item_id'].isin(not_sales)]
     
     # Уберем не интересные для рекоммендаций категории (department)
-    not_int = popularity[popularity['quantity'] < 1].item_id.tolist()
-    data = data[~data['item_id'].isin(not_int)]
+    if item_features is not None:
+        department_size = pd.DataFrame(item_features. \
+                                       groupby('department')['item_id'].nunique(). \
+                                       sort_values(ascending=False)).reset_index()
+
+        department_size.columns = ['department', 'n_items']
+        rare_departments = department_size[department_size['n_items'] < 100].department.tolist()
+        items_in_rare_departments = item_features[
+            item_features['department'].isin(rare_departments)].item_id.unique().tolist()
+
+        data = data[~data['item_id'].isin(items_in_rare_departments)]
     
-    # Уберем слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб. 
-    sales_cheap = popularity[popularity['sales_value'] < 1].item_id.tolist()
+    # Уберем слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб.
+    data['price'] = data['sales_value'] / (np.maximum(data['quantity'], 1))
+    sales_cheap = popularity[popularity['price'] < 60].item_id.tolist()
     data = data[~data['item_id'].isin(sales_cheap)]
         
     # Уберем слишком дорогие товары
-    sales_exp = popularity[popularity['sales_value'] > 10].item_id.tolist()
+    sales_exp = popularity[popularity['price'] > 10000].item_id.tolist()
     data = data[~data['item_id'].isin(sales_exp)]
     
     return data
