a531005edf13ed8717ffc0f3eac1ab206ddb3142
Alexander <59419330+Alexv035@users.noreply.github.com>
2021-05-20 20:00:54 +0300

    Update util.py

--- Lesson4/util.py
+++ Lesson4/util.py
@@ -13,14 +13,20 @@ def prefilter_items(data):
     data = data[~data['item_id'].isin(top_notpopular)]
     
     # Уберем товары, которые не продавались за последние 12 месяцев
-    not_sales = popularity[popularity['week_no'] > 52].item_id.tolist()
-    data = data[~data['item_id'].isin(top_notpopular)]
+    not_sales = popularity[popularity['trans_time'] > 365].item_id.tolist()
+    data = data[~data['item_id'].isin(not_sales)]
     
     # Уберем не интересные для рекоммендаций категории (department)
+    not_int = popularity[popularity['quantity'] < 1].item_id.tolist()
+    data = data[~data['item_id'].isin(not_int)]
     
     # Уберем слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб. 
-    
-    # Уберем слишком дорогие товарыs
+    sales_cheap = popularity[popularity['sales_value'] < 1].item_id.tolist()
+    data = data[~data['item_id'].isin(sales_cheap)]
+        
+    # Уберем слишком дорогие товары
+    sales_exp = popularity[popularity['sales_value'] > 10].item_id.tolist()
+    data = data[~data['item_id'].isin(sales_exp)]
     
     # ...
     
